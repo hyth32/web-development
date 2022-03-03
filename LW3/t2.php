b6ae1c3e4fc3ebdebd7763ed33951b4acf934a48
hyth32 <79153074+hyth32@users.noreply.github.com>
2022-03-03 08:58:06 +0300

    Add files via upload

---
+++ LW3/t2.php
@@ -0,0 +1,29 @@
+﻿<?php
+    header("Content-Type: text/plain");
+    $text = trim($_GET['identifier']);
+    $arrayText = str_split($text);
+    $answer = 'Не введен ключ или не присвоено значение';
+
+    if($text = trim($_GET['identifier']))
+    {
+        if(ctype_alpha($text))
+        {
+            $answer = 'Yes';
+            echo 'Идентификатор состоит из буквенных символов: ';
+        }
+        else
+        {
+            if(is_numeric($arrayText[0]))
+            {
+                $answer = 'No';
+                echo 'Идентификатор начинается с цифры: ';
+            }
+            else
+            {
+                $answer = 'Yes';
+                echo 'Идентификатор содержит цифру, но не начинается с нее: ';
+            }
+        }
+    }
+
+    echo $answer . '.';
